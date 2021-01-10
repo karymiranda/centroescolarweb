304ec1ee72a758d74fd97f76c70d223ed7b5937a
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-01-10 10:40:37 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,28 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'basesantamaria' );
+//define( 'DB_NAME', 'basesantamaria' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+//define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+//define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+//define( 'DB_HOST', 'localhost' );
+
+define( 'DB_NAME', 'Yu9VssY8uP' );
+
+/** MySQL database username */
+define( 'DB_USER', 'Yu9VssY8uP' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'NLcGgNnqFh' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'remotemysql.com' );
+
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
