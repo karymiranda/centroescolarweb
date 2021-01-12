a46e51507403f5b34cb9b6fcec88b31dbf9d3d55
karymiranda <36169783+karymiranda@users.noreply.github.com>
2021-01-11 21:46:59 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -90,7 +90,7 @@ $table_prefix = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'CONCATENATE_SCRIPTS', false );
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
